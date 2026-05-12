2ad2cd2154114f3e9f1bb86821a5ff4f2cb3e477
PR <ponnurajan.1314@gmail.com>
2026-05-12 12:13:03 +0530

    Oauth Added to access gmail

---
+++ auth/authenticate.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const { google } = require("googleapis");
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Access Scopes
+const SCOPES = process.env.GMAIL_SCOPES.split(",");
+//Credentials
+const CREDENTIALS_PATH = path.join(__dirname, "credentials.json");
+//tokens
+const TOKEN_PATH = path.join(__dirname, "token.json");
+
+async function authenticate() {
+  const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH));
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0],
+  );
+
+  // If token already exists, use it
+  if (fs.existsSync(TOKEN_PATH)) {
+    const token = JSON.parse(fs.readFileSync(TOKEN_PATH));
+    oAuth2Client.setCredentials(token);
+    console.log("Already authenticated.");
+    return oAuth2Client;
+  }
+
+  // First time — generate auth URL
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  console.log("\n🔗 Open this URL in your browser:\n");
+  console.log(authUrl);
+  console.log("\n");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve, reject) => {
+    rl.question("Paste the authorization code here: ", async (code) => {
+      rl.close();
+      try {
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+        console.log("Token saved to auth/token.json");
+        resolve(oAuth2Client);
+      } catch (err) {
+        reject("Error retrieving token: " + err);
+      }
+    });
+  });
+}
+
+authenticate();
