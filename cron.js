a0931cd4fa08aab92f85d19cbd5aebb793bd2a06
PR <ponnurajan.1314@gmail.com>
2026-05-15 10:59:49 +0530

    Refactoring comment lines.

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ import cron from 'node-cron';
 import runAgent from './src/agent.js';
 
 // Runs two times per day
-// Cron syntax: '*/5 * * * *' = every 5 minutes
+//API calls - 9am and 6pm
 cron.schedule('0 9,18 * * *', async () => {
   console.log(`[${new Date().toISOString()}] Running agent...`);
   await runAgent();
