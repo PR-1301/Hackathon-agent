cf19d1d834ae139e9dd82c2577ad10ee1df0ef80
PR <ponnurajan.1314@gmail.com>
2026-05-19 21:19:39 +0530

    fix: minor changes

--- models/ProcessedEmail.model.js
+++ models/ProcessedEmail.model.js
@@ -1,9 +1,5 @@
 import mongoose from 'mongoose';
 
-
-
-// Defines the schema for storing processed email UIDs
-// We only need the UID and when it was processed
 const processedEmailSchema = new mongoose.Schema({
   uid: { type: String, required: true, unique: true },
   processed_at: { type: Date, default: Date.now },
