06f05c0e4b0021117752a0b01167900d395d5f35
PR <ponnurajan.1314@gmail.com>
2026-05-26 13:45:57 +0530

    fix: auth

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ import cron from 'node-cron';
 import runAgent from './src/agent.js';
 
 // Runs two times per day
-//API calls - 9am and 6pm IST
+//API calls - 9am and 6pm IST 
 cron.schedule('30 3,12 * * *', async () => {
   console.log(`[${new Date().toISOString()}] Running agent...`);
   await runAgent();
